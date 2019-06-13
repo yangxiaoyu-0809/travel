5293ba292fe6abb922a8e788e2115bff9162af3b
yangxiaoyu <1150055925@qq.com>
2019-06-13 21:19:41 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return{
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/98/0a2e0bd8eb0c0802.png_750x200_0a34d4a8.png'
+      }, {
+        id: '0002',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/2df3ba0f23801d7ed5607f4767a18482.jpg_750x200_b4d6aa93.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background :#fff
+  .wrapper
+    width :100%
+    overflow hidden
+    height 0
+    padding-bottom: 27.25%
+    background :#eee
+    .swiper-img
+      width :100%
+</style>
